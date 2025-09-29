4b84f5f75ca1cb28db0bc3d1d2e41fa9a47f842b
Sylwia Vargas <sylwia.vargas@gmail.com>
2025-09-29 21:10:13 +0100

    improves nav

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "PostHog SuperDay",
-  tagline: "Good luck and have fun!",
+  tagline: "Thank you for today!",
   favicon: "img/favicon.ico",
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
